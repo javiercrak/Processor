3354d147f9af0117f3eeb901fb83e38496d2a96e
Servio Andrés Pantoja Rosero <Shd_cristo@hotmail.com>
2016-03-13 16:04:53 -0500

    Subia de alternative y alternative excludent

---
+++ src/code/Multimedia.java
@@ -0,0 +1,27 @@
+package code;
+
+@AlternativeNoExcludent(featureName="Multimedia")
+public class Multimedia {
+
+	public Multimedia(){}
+
+	@OptionFeature(featureName="Reproducir Video")
+	private void reproducirVideo(){
+
+	}
+	
+	@OptionFeature(featureName="Grabar Video")
+	private void grabarVideo(){
+
+	}
+	
+	@OptionFeature(featureName="Ver fotos")
+	private void verFotos(){
+
+	}
+	
+	@OptionFeature(featureName="Tomar fotos")
+	private void tomarFotos(){
+
+	}
+}
